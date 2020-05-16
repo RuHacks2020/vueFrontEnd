de493dbff183bd58823ef15420ab21596574c26f
Erio Hoti <erio.hoti@ryerson.ca>
2020-05-16 19:16:25 -0400

    HTML

---
+++ knockio-frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Navigation></Navigation>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Navigation from './components/Navigation'
+export default {
+  name: 'App',
+  components: {
+    'Navigation' : Navigation
+  }
+}
+</script>
+<style>
+  @import './assets/style.css';
+</style>
---
+++ knockio-frontend/src/components/Navigation.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id='nav'>
+        <nav>
+            <router-link v-for="routes in links"
+            :key="routes.id"
+            :to="routes.page">{{routes.text}}</router-link>
+        </nav>       
+    </div>
+</template>
+<script>
+export default {
+    name: 'Navigation',
+    data(){
+        return{
+            links: [
+                {
+                    id:0,
+                    text: 'KnockIO',
+                    page:'/'
+                    },
+                    {
+                    id:1,
+                    text:'Home',
+                    page:'/Home'
+                    },
+                    {
+                    id:2,
+                    text:'About',
+                    page:'/About'
+                    },
+                ]
+            }
+        }
+}
+</script>
